0d22fe14666008cde0a87b751fb7eb4819dd901d
sajidali <mohammedsajid@gmail.com>
2024-08-28 14:52:18 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://66ceeb4064690317729637e6--lucent-centaur-108369.netlify.app',
     credentials: true
 }));
 database();
